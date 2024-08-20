36f67188d62b3bb9991f5673f4f8f9a5e1711175
kabasunny <dxbqd553@yahoo.co.jp>
2024-08-20 13:06:58 +0900

    b_1

---
+++ backend/router/router.go
@@ -0,0 +1,43 @@
+package router
+
+import (
+	"gin-practice/controllers"
+	"gin-practice/middlewares"
+	"gin-practice/repositories"
+	"gin-practice/services"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupRouter(db *gorm.DB) *gin.Engine {
+
+	itemRepository := repositories.NewItemRepository(db)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controllers.NewAuthController(authService)
+
+	r := gin.Default()
+
+	r.Use(cors.Default()) //フロントとやり取りする場合は設定したほうがよい。
+
+	itemRouter := r.Group("/items")
+	itemRouterWithAuth := r.Group("/items", middlewares.AuthMiddleware(authService))
+	authRouter := r.Group("/auth")
+
+	itemRouter.GET("", itemController.FindAll)
+	itemRouterWithAuth.GET("/:id", itemController.FindById)
+	itemRouterWithAuth.POST("", itemController.Create)
+	itemRouterWithAuth.PUT("/:id", itemController.Update)
+	itemRouterWithAuth.DELETE("/:id", itemController.Delete)
+
+	authRouter.POST("/signup", authController.Signup)
+	authRouter.POST("/login", authController.Login)
+
+	return r
+
+}
